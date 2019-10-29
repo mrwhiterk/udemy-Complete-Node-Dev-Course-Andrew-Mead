a39541943f92d2268d0a7525828be4ade1f1379a
Ryan White <mrwhiterk@gmail.com>
2019-10-28 21:49:47 -0400

    finished 4-callbacks.js

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -8,25 +8,36 @@ const { API_KEY, MAP_BOX_API_KEY } = process.env
 const url = `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?units=us`
 
 request({ url, json: true }, (err, res) => {
-  const {
-    timezone,
-    currently: { temperature, precipProbability },
-    daily: {
-      data: [{ summary }]
-    }
-  } = res.body
-  
-  console.log(
-    chalk.green.inverse(
-      `\n${summary} It is currently ${temperature} degrees in ${timezone.split('/')[1].replace('_',' ')}. There is a ${precipProbability}% chance of rain.\n`
+  if (err) {
+    console.log('unable to connect to weather service')
+  } else if (res.body.error) {
+    console.log('unable to find location')
+  } else {
+    const {
+      timezone,
+      currently: { temperature, precipProbability },
+      daily: {
+        data: [{ summary }]
+      }
+    } = res.body
+    
+    console.log(
+      chalk.green.inverse(
+        `\n${summary} It is currently ${temperature} degrees in ${timezone.split('/')[1].replace('_',' ')}. There is a ${precipProbability}% chance of rain.\n`
+      )
     )
-  )
+  }
 })
 
 let mapBoxUrl =
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${MAP_BOX_API_KEY}&limit=1`
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/7Los%20Angeles.json?access_token=${MAP_BOX_API_KEY}&limit=1`
 
 request({ url: mapBoxUrl, json: true }, (err, res) => {
+  if (err) {
+    return console.log('unable to connect to map service')
+  } else if (!res.body.features.length) {
+    return console.log('unable to find location. Try again with different term')
+  }
   const [ { center: [long, lat] } ] = res.body.features
   console.log(chalk.green('longitude', long, '\nlatitude', lat))
 })
