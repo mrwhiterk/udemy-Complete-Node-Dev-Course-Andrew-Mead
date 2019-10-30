ad98b4ddabe8d90427c8af1eb685775fb32814d3
Ryan White <mrwhiterk@gmail.com>
2019-10-29 22:43:29 -0400

    started making hbs templates

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -3,8 +3,29 @@ const path = require('path')
 const app = express()
 
 // __filename  - prints full path of file
-
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name:'ryan'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'ryan'
+  })
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'ryan'
+  })
+});
 
 app.get('/weather', (req, res) => {
   res.send({ forecast: 'hot', location: 'bmore'})
