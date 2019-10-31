2052446f62f445e81169c9dd25a829808a4ab1a9
Ryan White <mrwhiterk@gmail.com>
2019-10-31 01:04:34 -0400

    started working with query parameters

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -40,7 +40,27 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-  res.send({ forecast: 'hot', location: 'bmore' })
+  if (!req.query.address) {
+    return res.send({ 
+      error: 'You must provide address'
+    })
+  }
+
+  res.send({ forecast: 'It is snowing', location: 'Philadelphia', address: req.query.address })
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  console.log(req.query)
+
+  res.send({
+    products: []
+  })
 })
 
 app.get('/help/*', (req, res) => {
