21f1b5fe5649b36bb3f320cdcb8d87913b993739
Ryan White <mrwhiterk@gmail.com>
2019-11-01 16:55:35 -0400

    got fetch request working with form submit

--- node-course/web-server/public/js/app.js
+++ node-course/web-server/public/js/app.js
@@ -1,1 +1,17 @@
-console.log('js loaded');
+const weatherForm = document.querySelector('form')
+
+weatherForm.addEventListener('submit', async e => {
+  e.preventDefault()
+
+  let { value } = weatherForm.querySelector('input')
+
+  const response = await fetch(`http://localhost:3000/weather?address=${value}`)
+  const data = await response.json()
+
+  if (data.error) {
+    return (document.querySelector('p').innerHTML = data.error)
+  }
+
+  document.querySelector('p').innerHTML = `${data.location} \n ${data.forecast}`
+
+})
