986c840fffdc01d88e73c297780e39fb81c8a418
Ryan White <mrwhiterk@gmail.com>
2019-11-04 18:55:48 -0500

    added user show route

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -19,6 +19,30 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users)
+    })
+    .catch(e => {
+      res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params
+  User.findById(id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send()
+      }
+      res.send(user)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
   task
