de0b6bfaec7128b8520c4675889e97a446207ab7
Ryan White <mrwhiterk@gmail.com>
2019-11-04 18:36:48 -0500

    created user/task post routes

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -45,18 +45,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 //   }
 // })
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
-
 // const grocery = new Task({
 //   description: 'get groceries'
 // })
