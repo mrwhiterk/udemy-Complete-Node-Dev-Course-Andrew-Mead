91e7081165b4f0934badaf5ca519e1baabb94593
Ryan White <mrwhiterk@gmail.com>
2019-11-04 17:56:17 -0500

    created first post route

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -1,49 +1,49 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+// const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('email is invalid')
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error("password cannot include the string 'password'")
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number')
-      }
-    }
-  }
-})
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error('email is invalid')
+//       }
+//     }
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minlength: 7,
+//     validate(value) {
+//       if (value.toLowerCase().includes('password')) {
+//         throw new Error("password cannot include the string 'password'")
+//       }
+//     }
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error('Age must be a positive number')
+//       }
+//     }
+//   }
+// })
 
 const Task = mongoose.model('Task', {
   description: {
@@ -57,16 +57,16 @@ const Task = mongoose.model('Task', {
   }
 })
 
-const grocery = new Task({
-  description: 'get groceries'
-})
+// const grocery = new Task({
+//   description: 'get groceries'
+// })
 
-grocery
-  .save()
-  .then(result => {
-    console.log(result)
-  })
-  .catch(err => console.log(err))
+// grocery
+//   .save()
+//   .then(result => {
+//     console.log(result)
+//   })
+//   .catch(err => console.log(err))
 
 // const me = new User({
 //   name: 'mike',
