60608cccc99fc8b4c4d0201cb70d0f0eae2b0d2a
Ryan White <mrwhiterk@gmail.com>
2019-11-04 15:48:39 -0500

    finished validation and sanitization section

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -18,9 +18,19 @@ const User = mongoose.model('User', {
     trim: true,
     lowercase: true,
     validate(value) {
-    
       if (!validator.isEmail(value)) {
         throw new Error('email is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error("password cannot include the string 'password'")
       }
     }
   },
@@ -35,36 +45,39 @@ const User = mongoose.model('User', {
   }
 })
 
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String
-//   },
-//   completed: {
-//     type: Boolean
-//   }
-// })
-
-// const grocery = new Task({
-//   description: 'get groceries',
-//   completed: false
-// })
-
-// grocery
-//   .save()
-//   .then(result => {
-//     console.log(result)
-//   })
-//   .catch(err => console.log(err))
-
-const me = new User({
-  name: 'mike',
-  email: 'mikeEMAIL@mead.io'
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 })
 
-me.save()
-  .then(() => {
-    console.log(me)
+const grocery = new Task({
+  description: 'get groceries'
+})
+
+grocery
+  .save()
+  .then(result => {
+    console.log(result)
   })
-  .catch(err => {
-    console.log('err', err)
-  })
+  .catch(err => console.log(err))
+
+// const me = new User({
+//   name: 'mike',
+//   email: 'mikeEMAIL@mead.io',
+//   password: 'abord'
+// })
+
+// me.save()
+//   .then(() => {
+//     console.log(me)
+//   })
+//   .catch(err => {
+//     console.log('err', err)
+//   })
