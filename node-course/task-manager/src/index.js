28b1d08491549a3e8ece49b2754d8f825f32a675
Ryan White <mrwhiterk@gmail.com>
2019-11-05 12:06:07 -0500

    just finished adding validation to user patch route

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -7,71 +7,91 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
-  const user = new User(req.body)
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch(err => {
-      res.status(400).send(err)
-    })
+app.get('/users', async (req, res) => {
+  try {
+    let users = await User.find()
+    if (!user) return res.status(404).send()
+    res.send(users)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users)
-    })
-    .catch(e => {
-      res.status(500).send()
-    })
-})
-
-app.get('/tasks', (req, res) => {
-  Task.find()
-    .then(tasks => {
-      res.send(tasks)
-    })
-    .then(e => res.status(500).send(e))
-})
-
-app.get('/tasks/:id', (req, res) => {
-  Task.findById(req.params.id)
-    .then(task => {
-      if (!task) return res.send('no task with that id')
-      res.send(task)
-    })
-    .catch(e => {
-      res.status(400).send()
-    })
-})
-
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id } = req.params
-  User.findById(id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send()
-      }
-      res.send(user)
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    })
+  try {
+    let user = await User.findById(id)
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/users', async (req, res) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every(item => allowedUpdates.includes(item))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!'})
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+app.get('/tasks', async (req, res) => {
+  try {
+    let tasks = await Task.find()
+    res.send(tasks)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    let task = await Task.findById(req.params.id)
+    if (!task) return res.send('no task with that id')
+    res.send(task)
+  } catch (e) {
+    res.status(400).send()
+  }
+})
+
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task)
-    })
-    .catch(err => {
-      res.status(400).send(err)
-    })
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(err)
+  }
 })
 
 app.listen(port, () => {
