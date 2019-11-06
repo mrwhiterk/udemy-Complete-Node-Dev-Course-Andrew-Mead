d084a50f5ebd7d904306e1f6e6dfcd1a7ab16134
Ryan White <mrwhiterk@gmail.com>
2019-11-06 14:53:28 -0500

    added filtering to get tasks request

--- node-course/task-manager/src/routers/task.js
+++ node-course/task-manager/src/routers/task.js
@@ -3,13 +3,25 @@ const router = new express.Router()
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 
+// GET /tasks?completed=false
 router.get('/tasks', auth, async (req, res) => {
+  const match = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
   try {
     // or const tasks = await Task.find({ owner: req.user._id})
-    await req.user.populate('tasks').execPopulate()
+    await req.user
+      .populate({
+        path: 'tasks',
+        match
+      })
+      .execPopulate()
     res.send(req.user.tasks)
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
 
@@ -53,8 +65,8 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
   try {
     // const task = await Task.findById(req.params.id)
-    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
-    
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
     if (!task) {
       return res.status(404).send()
     }
@@ -71,7 +83,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id})
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    })
 
     if (!task) {
       return res.status(404).send()
