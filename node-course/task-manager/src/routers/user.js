1b89a5f50f08f4a66d83eb100e351689d9f8a02b
Ryan White <mrwhiterk@gmail.com>
2019-11-06 10:40:20 -0500

    removed show user by id and added show/me update/me

--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -7,18 +7,18 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id', auth, async (req, res) => {
-  const { id } = req.params
-  try {
-    let user = await User.findById(id)
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.send(user)
-  } catch (e) {
-    res.status(500).send(e)
-  }
-})
+// router.get('/users/:id', auth, async (req, res) => {
+//   const { id } = req.params
+//   try {
+//     let user = await User.findById(id)
+//     if (!user) {
+//       return res.status(404).send()
+//     }
+//     res.send(user)
+//   } catch (e) {
+//     res.status(500).send(e)
+//   }
+// })
 
 //signup
 router.post('/users', async (req, res) => {
@@ -75,7 +75,7 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 })
 
 //update
-router.patch('/users/:id', auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every(item => allowedUpdates.includes(item))
@@ -85,33 +85,25 @@ router.patch('/users/:id', auth, async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id)
 
-    updates.forEach(update => (user[update] = req.body[update]))
+    updates.forEach(update => (req.user[update] = req.body[update]))
 
-    await user.save()
+    await req.user.save()
 
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
+    res.send(req.user)
   } catch (error) {
     res.status(400).send(error)
   }
 })
 
 //delete
-router.delete('/users/:id', auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
-    if (!user) {
-      return res.status(400).send()
-    }
+    await req.user.remove()
 
-    res.send(user)
+    res.send(req.user)
   } catch (error) {
-    res.status(400).send(error)
+    res.status(500).send(error)
   }
 })
 
