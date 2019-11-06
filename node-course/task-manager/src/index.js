2b60b0fa35c02a641eeaf4980d1fb2cf4255b539
Ryan White <mrwhiterk@gmail.com>
2019-11-06 13:19:54 -0500

    got delete route working so that users can only delete todos they own

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -26,61 +26,59 @@ const main = async () => {
   // await task.populate('owner').execPopulate()
   // console.log(task)
 
-  const user = await User.findById('5dc2ebb673c34fa77593f987')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-  
+  // const user = await User.findById('5dc2ebb673c34fa77593f987')
+  // await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
 }
 
 main()
 
-
 /** This is how toJSON works */
-  // const pet = {
-  //   name: 'Hal'
-  // }
+// const pet = {
+//   name: 'Hal'
+// }
 
-  // pet.toJSON = function (params) {
-  //  console.log(this) 
-  //   return {}
-  // }
+// pet.toJSON = function (params) {
+//  console.log(this)
+//   return {}
+// }
 
-  // console.log(JSON.stringify(pet))
+// console.log(JSON.stringify(pet))
 
 /** jwt basics */
 
-  // const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-  // const jsonWebFunc = async () => {
-  //   const token = jwt.sign(
-  //     {
-  //       _id: 'abc123'
-  //     },
-  //     'thisismynewcourse',
-  //     { expiresIn: '7 days' }
-  //   )
+// const jsonWebFunc = async () => {
+//   const token = jwt.sign(
+//     {
+//       _id: 'abc123'
+//     },
+//     'thisismynewcourse',
+//     { expiresIn: '7 days' }
+//   )
 
-  //   console.log(token)
+//   console.log(token)
 
-  //   const data = jwt.verify(token, 'thisismynewcourse')
-  //   console.log(data)
-  // }
+//   const data = jwt.verify(token, 'thisismynewcourse')
+//   console.log(data)
+// }
 
-  // jsonWebFunc()
+// jsonWebFunc()
 
 /** Bcrypt basics */
 
-  // const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
-  // const myFunc = async () => {
-  //   const password = 'red1234!'
+// const myFunc = async () => {
+//   const password = 'red1234!'
 
-  //   const hashedPassword = await bcrypt.hash(password, 8)
+//   const hashedPassword = await bcrypt.hash(password, 8)
 
-  //   console.log(password, hashedPassword)
+//   console.log(password, hashedPassword)
 
-  //   const isMatch = await bcrypt.compare('red1234!', hashedPassword)
-  //   console.log(isMatch)
-  // }
+//   const isMatch = await bcrypt.compare('red1234!', hashedPassword)
+//   console.log(isMatch)
+// }
 
-  // myFunc()
+// myFunc()
--- node-course/task-manager/src/routers/task.js
+++ node-course/task-manager/src/routers/task.js
@@ -5,20 +5,24 @@ const auth = require('../middleware/auth')
 
 router.get('/tasks', auth, async (req, res) => {
   try {
-    let tasks = await Task.find()
-    res.send(tasks)
+    // or const tasks = await Task.find({ owner: req.user._id})
+    await req.user.populate('tasks').execPopulate()
+    res.send(req.user.tasks)
   } catch (e) {
     res.status(500).send(e)
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
+  const _id = req.params.id
   try {
-    let task = await Task.findById(req.params.id)
-    if (!task) return res.send('no task with that id')
+    const task = await Task.findOne({ _id, owner: req.user._id })
+
+    if (!task) return res.status(404).send()
+
     res.send(task)
   } catch (e) {
-    res.status(400).send()
+    res.status(500).send()
   }
 })
 
@@ -35,7 +39,7 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
 
@@ -48,28 +52,32 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id)
+    // const task = await Task.findById(req.params.id)
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+    
+    if (!task) {
+      return res.status(404).send()
+    }
     updates.forEach(update => {
       return (task[update] = req.body[update])
     })
 
     await task.save()
-
-    if (!task) {
-      return res.status(404).send()
-    }
     res.send(task)
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id)
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id})
+
     if (!task) {
       return res.status(404).send()
     }
+    // or find and await task.remove()
+
     res.send(task)
   } catch (error) {
     res.status(400).send(error)
