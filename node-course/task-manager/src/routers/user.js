e89a8c1af8eeecf7cc14eaa23717a029aec6cf2f
Ryan White <mrwhiterk@gmail.com>
2019-11-06 16:58:30 -0500

    setup upload validation for avatar

--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -106,7 +106,17 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-  dest: 'avatars'
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an jpg, jpeg, png'))
+    }
+    cb(undefined, true)
+  }
+
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
