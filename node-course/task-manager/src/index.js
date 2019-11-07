52c50e55a52b1e23d33671264ef8afdf3594bba1
Ryan White <mrwhiterk@gmail.com>
2019-11-07 11:46:58 -0500

    cropping and reformatting avatar before save to db

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -6,37 +6,37 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
+// const multer = require('multer')
 
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error('Please upload a Word document'))
-    }
+// const upload = multer({
+//   dest: 'images',
+//   limits: {
+//     fileSize: 1000000
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(doc|docx)$/)) {
+//       return cb(new Error('Please upload a Word document'))
+//     }
 
-    cb(undefined, true)
+//     cb(undefined, true)
 
-    // cb(new Error('File must be a PDF')) 
-    // cb(undefined, true)
-    // cb(undefined, false)
-  }
-})
+//     // cb(new Error('File must be a PDF')) 
+//     // cb(undefined, true)
+//     // cb(undefined, false)
+//   }
+// })
 
 // const errorMiddleware = (req, res, next) => {
 //   throw new Error('err from errorMiddleware')
 // }
 
 
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-}
-)
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//   res.send()
+// }, (error, req, res, next) => {
+//   res.status(400).send({ error: error.message })
+// }
+// )
 
 const morgan = require('morgan')
 
--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
 const multer = require('multer')
+const sharp = require('sharp')
 
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
@@ -122,7 +123,12 @@ router.post(
   auth,
   upload.single('avatar'),
   async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer()
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
   },
@@ -147,7 +153,7 @@ router.get('/users/:id/avatar', async (req, res) => {
       throw new Error()
     }
 
-    res.set('Content-Type','image/jpg')
+    res.set('Content-Type', 'image/png')
     res.send(user.avatar)
   } catch (error) {
     res.status(404).send()
